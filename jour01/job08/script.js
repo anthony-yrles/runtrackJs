25b6d833b160bdfee65331da9c3452e99749cf26
Sluchai40 <mabbolos8@gmail.com>
2024-04-15 10:27:57 +0200

    [DONE] Job 08 finished

---
+++ jour01/job08/script.js
@@ -0,0 +1,15 @@
+function sommeNombresPremiers(nombre_1, nombre_2) {
+    for(let i = 2; i < nombre_1; i++)
+        if(nombre_1%i === 0) 
+            return false;
+        for(let i = 2; i < nombre_2; i++)
+            if(nombre_2%i === 0) 
+                return false;
+    return nombre_1 + nombre_2;
+}
+
+console.log(sommeNombresPremiers(2, 3));
+console.log(sommeNombresPremiers(2, 4));
+console.log(sommeNombresPremiers(2, 5));
+console.log(sommeNombresPremiers(6432, 6433));
+console.log(sommeNombresPremiers(17, 17));
