31107a4e9666f231f09dd6327e33bc096b66dde1
Sluchai40 <mabbolos8@gmail.com>
2024-04-17 11:18:41 +0200

    [DONE] Job01 finished

---
+++ jour03/job01/script.js
@@ -0,0 +1,14 @@
+// Creation of the script
+$(document).ready(function() {
+    // Button show selection
+    $("#showButton").click(function() {
+        // Texte show selection
+        $("#text").show();
+    });
+
+    // Button hide selection
+    $("#hideButton").click(function() {
+        // Texte hide selection
+        $("#text").hide();
+    });
+});
